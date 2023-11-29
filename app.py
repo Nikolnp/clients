e793a5da35548c1351521e084199c6390f9a486e
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2023-11-29 16:57:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 
 # Load your dataset
 df = pd.read_csv('C:/Users/Nikoleta/Source/Repos/clients/data.csv')
@@ -12,12 +12,3 @@ st.title('Data Visualization with Streamlit')
 st.subheader('Raw Data')
 st.table(df)
 
-# Visualization: Bar Chart
-st.subheader('Bar Chart - Number of Projects by Client')
-bar_chart = px.bar(df, x='Client', title='Number of Projects by Client')
-st.plotly_chart(bar_chart)
-
-# Visualization: Pie Chart
-st.subheader('Pie Chart - Distribution of Implementation Types')
-pie_chart = px.pie(df, names='Implementation Type', title='Distribution of Implementation Types')
-st.plotly_chart(pie_chart)
