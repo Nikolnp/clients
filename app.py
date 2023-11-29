01a97ec0428541d52a1c42323bdaf3e5f2abca9f
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2023-11-29 16:47:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-# Import necessary libraries
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
+import plotly.express as px
 
 # Load your dataset
-df = pd.read_csv('data.csv')
+df = pd.read_csv('C:/Users/Nikoleta/Source/Repos/clients/data.csv')
 
 # Streamlit app
 st.title('Data Visualization with Streamlit')
 
 # Show the raw data
 st.subheader('Raw Data')
-st.write(df)
+st.table(df)
 
 # Visualization: Bar Chart
 st.subheader('Bar Chart - Number of Projects by Client')
-bar_data = df['Client'].value_counts()
-bar_chart = go.Figure(go.Bar(x=bar_data.index, y=bar_data.values))
-bar_chart.update_layout(title='Number of Projects by Client', xaxis_title='Client', yaxis_title='Number of Projects')
+bar_chart = px.bar(df, x='Client', title='Number of Projects by Client')
 st.plotly_chart(bar_chart)
 
 # Visualization: Pie Chart
 st.subheader('Pie Chart - Distribution of Implementation Types')
-pie_data = df['Implementation Type'].value_counts()
-pie_chart = go.Figure(go.Pie(labels=pie_data.index, values=pie_data.values))
-pie_chart.update_layout(title='Distribution of Implementation Types')
+pie_chart = px.pie(df, names='Implementation Type', title='Distribution of Implementation Types')
 st.plotly_chart(pie_chart)
